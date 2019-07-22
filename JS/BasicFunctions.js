a6e2211f88aac4114a260e0c66e5c63383a7f74e
7Ravenz <7Ravenz@users.noreply.github.com>
2019-07-22 09:40:49 -0300

    Language is remembered through pages

--- JS/BasicFunctions.js
+++ JS/BasicFunctions.js
@@ -8,8 +8,17 @@ var changeLanguage = function(lang) {
     if(lang == "en") {
         $(".en").show();
         $("#French_btn").show();
+        localStorage.setItem('language', 'en');
     } else if(lang == "fr") {
         $(".fr").show();
         $("#English_btn").show();
+        localStorage.setItem('language', 'fr');
     }
 };
+
+var initLanguage = function(){
+    if (localStorage.getItem('language') == null)
+        changeLanguage('en')
+    else
+        changeLanguage(localStorage.getItem('language'))
+}
