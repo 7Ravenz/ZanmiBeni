1364d6864e2650dce70f96d109e0be6fc3a50bfc
7Ravenz <7Ravenz@users.noreply.github.com>
2019-07-25 12:02:52 -0300

    Spacing changes

--- JS/ScrollButton.js
+++ JS/ScrollButton.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     document.getElementById("ScrollButton").style.display = "block";
   } else {
     document.getElementById("ScrollButton").style.display = "none";
