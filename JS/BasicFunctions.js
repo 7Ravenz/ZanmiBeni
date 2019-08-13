d9a6fb16fbdd2fa7a97b626175f0ab1ce7bc0aaa
7Ravenz <7Ravenz@users.noreply.github.com>
2019-08-12 21:59:27 -0300

    createChildren() function created

--- JS/BasicFunctions.js
+++ JS/BasicFunctions.js
@@ -24,3 +24,46 @@ var switchLanguage = function() {
     else
         changeLanguage('en')
 }
+
+var kids = 
+[
+    {"imgfile":"Gallery5.JPG", "name":"Child 1",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"imgfile":"Gallery6.JPG", "name":"",
+    "enBio":"",
+    "frBio":""},
+
+    {"imgfile":"Whatever.JPG", "name":"", 
+    "enBio":"", 
+    "frBio":""}
+];
+
+var createChildren = function()
+{
+var htmlreturn = "";
+
+for(var i = 0 ; i < kids.length ; i++)
+{
+    htmlreturn += 
+    `
+    <div class="col s12 m6 l4">
+    <div class="card">
+        <div class="card-image">
+            <img src="../IMAGES/${kids[i].imgfile}" alt="${kids[i].name} picture">
+            <span class="card-title"> ${kids[i].name} </span>
+
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">attach_money</i></a>
+        </div>
+        <div class="card-content">
+            <span class="en" lang="en"> <p class="childInfo"> ${kids[i].enBio} </p> </span>
+            <span class="fr" lang="fr"> <p class="childInfo"> ${kids[i].frBio} </p> </span>
+        </div>
+    </div>
+</div>
+`
+}
+
+return htmlreturn;
+}
