90c38f770b2a0315ae24041130b04dddc672839c
7Ravenz <7Ravenz@users.noreply.github.com>
2019-08-21 13:21:21 -0300

    Create the children

--- JS/BasicFunctions.js
+++ JS/BasicFunctions.js
@@ -27,17 +27,141 @@ var switchLanguage = function() {
 
 var kids = 
 [
-    {"imgfile":"EGGS.JPG", "name":"Child 1",
+    {"name":"Andrecia",
     "enBio":"Child's Bio... Some English stuff!",
     "frBio":"Child's Bio... Des choses Françaises!"},
 
-    {"imgfile":"ARTS CENTER.JPG", "name":"",
+    {"name":"Betty",
     "enBio":"english",
     "frBio":"francais"},
 
-    {"imgfile":"DORMITORY.JPG", "name":"", 
+    {"name":"Carl", 
     "enBio":"english", 
-    "frBio":"francais"}
+    "frBio":"francais"},
+
+    {"name":"Christopher",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Claudia",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Davidson",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Dieuverson",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Gisele",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"James",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Laurent",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Lucie",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Marco",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Marcus",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Martine",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Merline",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Michalle",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Mishama",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Nephtalie",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Nicole",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Ogeline",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Pascale",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Peter John",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Peterson",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Rene",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Rosemercie",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Roudy",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Samuel",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Sebastian",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Sherika",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Steff",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Stephane",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Wadley",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Wendy",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
+
+    {"name":"Yveline",
+    "enBio":"Child's Bio... Some English stuff!",
+    "frBio":"Child's Bio... Des choses Françaises!"},
 ];
 
 var createChildren = function()
@@ -48,10 +172,10 @@ for(var i = 0 ; i < kids.length ; i++)
 {
     htmlreturn += 
     `
-    <div class="col s12 m6 l4">
+    <div class="col s12 m4 l3">
     <div class="card">
         <div class="card-image">
-            <img src="../IMAGES/${kids[i].imgfile}" alt="${kids[i].name} picture">
+            <img src="../KIDS/${kids[i].name}.jpg" alt="${kids[i].name} picture">
             <span class="card-title"> ${kids[i].name} </span>
 
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">attach_money</i></a>
