2481ad730bd1e5622e021f9ee945be463dccd63e
7Ravenz <7Ravenz@users.noreply.github.com>
2019-10-06 18:27:30 -0300

    fixed kids

--- JS/KidsMaker.js
+++ JS/KidsMaker.js
@@ -39,11 +39,11 @@ var kids =
     "enBio":"english",
     "frBio":"francais"},
 
-    {"name":"Carl, 8", 
+    {"name":"Carl", 
     "enBio":"Carl likes to play with his friends. Even with his mobility issues, he finds a way to keep up with his friends, and to participate in all activities. He says he loves to play ball. ", 
     "frBio":"Carl aime jouer avec ses amis. Malgré des difficultés de mobilité, Carl trouve moyen de suivre les autres et de participer aux activités avec ses pairs. Il dit adorer jouer au ballon. "},
 
-    {"name":"Christopher, 7",
+    {"name":"Christopher",
     "enBio":"Christopher likes to play basketball or dominos with his best friend Michaëlle. He does not mind helping with chores; he says he likes working in the kitchen or sweeping. ",
     "frBio":"Christopher aime jouer au basketball ou aux dominos avec sa meilleure amie Michaëlle. Il trouve aussi du plaisir à faire des tâches ménagères comme travailler dans la cuisine ou balayer. "},
 
@@ -55,7 +55,7 @@ var kids =
     "enBio":"Child's Bio... Some English stuff!",
     "frBio":"Child's Bio... Des choses Françaises!"},
 
-    {"name":"Dieuverson, 10",
+    {"name":"Dieuverson",
     "enBio":"Dieuverson, like most kids at Zamni Beni, likes to play soccer. During the games, he likes to be the goaltender. He also lies to play jumprope with his friends. ",
     "frBio":"Dieuverson, comme plusieurs enfants à Zamni Beni, aime jouer au football. Par contre, celui-ci aime bien être le gardien de buts lors des matchs. Il aime aussi sauter à la corde avec ses amis. "},
 
@@ -79,11 +79,11 @@ var kids =
     "enBio":"Child's Bio... Some English stuff!",
     "frBio":"Child's Bio... Des choses Françaises!"},
 
-    {"name":"James, 12",
+    {"name":"James",
     "enBio":"James adores school and doing his homework. When he’s older, he would like to become a math teacher in an elementary school. On top of this, he likes to play soccer with his friends at Zamni Beni. ",
     "frBio":"James adores school and doing his homework. When he’s older, he would like to become a math teacher in an elementary school. On top of this, he likes to play soccer with his friends at Zamni Beni. "},
 
-    {"name":"Laurent, 9",
+    {"name":"Laurent",
     "enBio":"Laurent loves math and wants to become a buisnessman. He loves doing school work, or playing jump rope and dominos.",
     "frBio":"Laurent est passionné des mathématiques et veut devenir un homme d’affaires. Il aime travailler sur ses travaux scolaires, ainsi que jouer à la corde ou aux dominos."},
     
@@ -115,7 +115,7 @@ var kids =
     "enBio":"Child's Bio... Some English stuff!",
     "frBio":"Child's Bio... Des choses Françaises!"},
 
-    {"name":"Michaelle (Micha), 9",
+    {"name":"Michalle",
     "enBio":"Michaëlle likes to play ball with her best friend Nicole. Her favorite meal is all-meat pizza. She says she loves to spend time with Nicole, even at meal time. ",
     "frBio":"Michaëlle adore jouer au ballon avec sa meilleure amie Nicole. Son repas préféré est de la pizza garnie de viande. Elle affirme qu’elle aime bien passer du temps avec Nicole, même pendant l’heure du repas."},
 
@@ -171,7 +171,7 @@ var kids =
     "enBio":"Child's Bio... Some English stuff!",
     "frBio":"Child's Bio... Des choses Françaises!"},
 
-    {"name":"Samuel, 8",
+    {"name":"Samuel",
     "enBio":"Samuel is super silly. He loves to laugh and to make others laugh. His favorite activity is drawing with his best friend Sébastien. When he grows up, Samuel wants to become a professional soccer player.",
     "frBio":"Samuel est très farfelu. Il aime bien rire et faire rire ses amis. Son activité préférée est de faire des dessins avec son ami Sébastien. Lorsqu’il sera grand, Samuel veut devenir un joueur de football professionnel. "},
 
